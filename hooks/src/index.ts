9455cc0d6c9a5005a652479f6a9b5147d299d6d0
Prasad Gurude <prasadgurude76@gmail.com>
2025-04-23 08:50:18 +0530

    init

---
+++ hooks/src/index.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import { PrismaClient } from "./generated/prisma";
+
+const client  =  new PrismaClient()
+
+const app = express();
+const port = 3000;
+
+// https://hooks.zapier.com/hooks/catch/22591153/2xizz0t/
+
+app.post("/hooks/catch/:userId/:zapId/",async (req, res) => {
+
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+    const body = req.body
+
+    // soter in deb a new trigger
+    await client.$transaction(async tx => {
+        const run = await client.zapRun.create({
+            data : {
+                zapId: zapId,
+                metadata:body
+            }
+        });
+
+        await client.zapRunOutbox.create({
+            data:{
+                zapRunId: run.id
+            }
+        })
+    })
+
+    // push it on to a queue {kafka / redis}
+    
+});
+
+app.listen(port,()=>{
+    console.log("server is up and running at port 3000")
+})
